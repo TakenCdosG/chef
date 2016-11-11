d640f540b2875aae99fb9a59901572c4472a5684
Adrián A. Morelos Henriquez <adrian.morelos@akendos.com>
2016-11-11 16:54:25 -0500

    Ajustes

--- wp-content/themes/travelify/woocommerce/global/breadcrumb.php
+++ wp-content/themes/travelify/woocommerce/global/breadcrumb.php
@@ -17,32 +17,50 @@
  * @see         woocommerce_breadcrumb()
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
-	exit;
+if (!defined('ABSPATH')) {
+  exit;
 }
 
-if ( ! empty( $breadcrumb ) ) {
+global $product;
+if (isset($product->id)) {
+  $defaults = array('fields' => 'ids');
+  $args = wp_parse_args(array(), $defaults);
+  $categories = wp_get_object_terms(array($product->id), 'product_cat');
+  //$category = reset($categories);
+  //dpm($result);
+  if (!empty($breadcrumb)) {
+    echo $wrap_before;
+    echo $before;
+    echo '<a href="' . esc_url(home_url('/')) . '">' . esc_html("Home") . '</a>';
+    echo $after;
+    echo $delimiter;
+    echo format_breadcrumb_category($categories, $before, $after, $delimiter, $product);
+    echo $wrap_after;
+  }
+} else {
+  if (!empty($breadcrumb)) {
 
-	echo $wrap_before;
+    echo $wrap_before;
 
-	foreach ( $breadcrumb as $key => $crumb ) {
+    foreach ($breadcrumb as $key => $crumb) {
 
-		echo $before;
+      echo $before;
 
-		if ( ! empty( $crumb[1] ) && sizeof( $breadcrumb ) !== $key + 1 ) {
-			echo '<a href="' . esc_url( $crumb[1] ) . '">' . esc_html( $crumb[0] ) . '</a>';
-		} else {
-			echo esc_html( $crumb[0] );
-		}
+      if (!empty($crumb[1]) && sizeof($breadcrumb) !== $key + 1) {
+        echo '<a href="' . esc_url($crumb[1]) . '">' . esc_html($crumb[0]) . '</a>';
+      } else {
+        echo esc_html($crumb[0]);
+      }
 
-		echo $after;
+      echo $after;
 
-		if ( sizeof( $breadcrumb ) !== $key + 1 ) {
-			echo $delimiter;
-		}
+      if (sizeof($breadcrumb) !== $key + 1) {
+        echo $delimiter;
+      }
 
-	}
+    }
 
-	echo $wrap_after;
+    echo $wrap_after;
 
+  }
 }
